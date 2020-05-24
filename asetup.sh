ced2095267fe0cfa8e8f00dce28d70086475f2fe
Fr3db3rt <fr3db3rt@gmail.com>
2020-05-24 17:26:06 +0200

    Update asetup.sh

--- asetup.sh
+++ asetup.sh
@@ -1,8 +1,14 @@
-echo Fore more information read source at 
+echo for more information read source at 
 echo https://wiki.archlinux.de/title/Anleitung_f%C3%BCr_Einsteiger
+echo
 
+echo load german keyboard settings
 loadkeys de
+echo
 
+echo list and format disk
+echo
 fdisk -l
+echo
 
-#fdisk /dev/sda
+fdisk /dev/sda
